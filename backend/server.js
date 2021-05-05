25830a661b8e4bd6296a164dd628965e1b51eca8
Bastian <bastian.bjorn@gmail.com>
2021-05-05 11:07:57 +0200

    added login site and logged in site aswell as frontend folder

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,32 @@
-const express = require('express');
-const lowdb = require('lowdb');
-const cookieParser = require ('cookie-parser');
+const express = require("express");
+const lowdb = require("lowdb");
+const cookieParser = require("cookie-parser");
 
-const FileSync = require('lowdb/adapters/FileSync');
-const adapter = new FileSync('users.json');
+const FileSync = require("lowdb/adapters/FileSync");
+const adapter = new FileSync("users.json");
 const database = lowdb(adapter);
 
 const app = express();
 
+app.use(express.json);
+app.use(express.static('../frontend'));
 
+app.post('/api/login', (request, response) => {
+  const credentials = request.body;
 
+  const user = database
+    .get('accounts')
+    .find({ username: credentials.username, password: credentials.password })
+    .value();
 
+  let result = { success: false };
 
-
-
-
-
+  if (user) {
+    result.success = true;
+  }
+  response.json(result);
+});
 
 app.listen(8000, () => {
-    console.log('Server Started');
+  console.log("Server Started");
 });
---
+++ frontend/index.js
@@ -0,0 +1,35 @@
+const usernameInput = document.querySelector('#username');
+const passwordInput = document.querySelector('#pass');
+const loginButton = document.querySelector('#login');
+
+async function login(user, pass) {
+    const obj = {
+        username: user,
+        password: pass
+    }
+
+    const response = await fetch('http://localhost:8000/api/login', {
+        method: 'POST',
+        body: JSON.stringify(obj),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    const data = await response.json
+   
+    console.log('Login:', data);
+
+    return await data:
+}
+
+
+loginButton.addEventListener('click', async() => {
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+
+    const loggedIn = await login(username, password);
+
+    if(loggedIn.success) {
+        location.href = 'http://localhost:8000/loggedIn.html'
+    }
+});
